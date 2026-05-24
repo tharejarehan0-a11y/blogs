112477fb57f97d19779b16ac31a6a3b18785ac84
Rehan-thareja <tharejarehan0@gmail.com>
2026-05-25 03:39:13 +0530

    added photobooths
    added framer motion library and did add photobooths with use of it

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Cursor from "@/components/cursor"; 
-
+import Photobooth from "@/components/photoholder";
 export default function Home() {
   return (
     <div>
@@ -10,12 +10,26 @@ export default function Home() {
       <li><a href="#">Contact</a></li>
     </nav>
       <Cursor />
+    <div className="flex justify-evenly items-center gap-25">
+    <div className="flex gap-5 justify-center item-center">
+    <Photobooth Image="/WhatsApp Image 2026-05-25 at 03.25.44.jpeg" rotation="350deg"/>
+    <Photobooth Image="/WhatsApp Image 2026-05-25 at 03.26.03.jpeg" rotation="20deg"/>
+    <Photobooth Image="/WhatsApp Image 2026-05-25 at 03.26.15.jpeg" rotation="350deg"/>
+  
+    </div>
     <main className="flex flex-col items-center justify-center bg-center mt-70 gap-10">
-      <h1 className="block text-7xl subpixel-antialiased font-fancy"> 
+      <h1 className="block text-5xl subpixel-antialiased font-fancy"> 
         Rehan Blogs
       </h1>
       <span className="block antialiased font-extrabold font-fancy text-2xl">A New way of thinking</span>
     </main>
+    <div className="flex gap-5 justify-center item-center">
+    <Photobooth Image="/WhatsApp Image 2026-05-25 at 03.26.32.jpeg" rotation="20deg"/>
+    <Photobooth Image="/WhatsApp Image 2026-05-25 at 03.26.43.jpeg" rotation="330deg"/>
+    <Photobooth Image="/WhatsApp Image 2026-05-25 at 03.27.22.jpeg" rotation="20deg"/>
+
+    </div>
+    </div>
     </div>
   );
 }
---
+++ components/photoholder.jsx
@@ -0,0 +1,21 @@
+"use client"
+import { motion } from 'motion/react'
+export default function Photobooth({ Image, rotation }) {
+    const style = {
+        transform: `rotate(${rotation})`,
+        transformOrigin: 'center',
+    }
+
+    return (
+        <>
+        <motion.div 
+        className="bg-amber-50 border-black border-2 inline-block pr-2 pl-2 pt-2 pb-6 mt-6" 
+        initial={{opacity:0,scale:0}}
+        animate={{ rotate:rotation,opacity:1,scale:1 }}
+        transition={{duration:2}}
+        >
+            <img src={Image} alt="image" height="300px" width="120px" className="border-2 border-black" />
+        </motion.div>
+        </>
+    )
+}
