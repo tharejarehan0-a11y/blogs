002dd3ff9e413008d15c78ef3e0678a770ffdbf8
Rehan-thareja <tharejarehan0@gmail.com>
2026-06-19 15:49:21 +0530

    changed the images
    Every page has now images related to it and the text also either no there stil or it has been improved

--- app/articles/Generation/page.jsx
+++ app/articles/Generation/page.jsx
@@ -14,41 +14,11 @@ export default function Generation(){
         </div>
         <div className="bg-black goto-end h-full">
         <div className="w-full flex flex-wrap m-0 justify-center items-center gap-40" >
-            <Card button="1" img="/pexels-gunnar-hoffmann-2154527493-34220116.jpg" />    
-            <Card button="1" img="/pexels-omron2003-33720059.jpg" />              
+            <Card button="1" img="/rod-long-y0OAmd_COUM-unsplash.jpg" />    
+            <Card button="1" img="/ekaterina-shakharova-L4nwL3195U0-unsplash.jpg" />              
         </div>
         <div className="text-white text-xl flex justify-center items-center p-16 font-article">
-            <div>
-                <div className='m-8 rounded-4xl p-16 border-8 border-white'>
-
-                    <h1 className="text-4xl mb-5">Overview</h1>
-                    This is litterally the most under rated option a student can look upto . It has no tution fees moreover the quality of education is one of the best in the world. It is the one of the bests in engineering and medical. There are some top tier schools in germany such as RWTH Aachen , TU     Universties who are ranked high in the QS rankings and are easier to get in compared to the     universties in the other countries. Germany ranks among the global leaders in medical education, with 43 public universities offering medical courses and 32 institutions appearing in the QS     World University Rankings 2026.
-
-                </div>
-                <div className=' m-8 rounded-4xl p-16 border-8 border-white'>
-
-                <h1 className='text-4xl mt-5 mb-5'>Requirements</h1>
-                <h2 className="text-3xl mt-3 mb-3 "> Undergrad </h2>
-                For Undergraduate courses in germany you wont find many scholarships . Some ways that you can cover your     fees are listed below:
-                <ul className='list-decimal mt-5 list-inside'>
-                    <li className='list-item'>Part-time Job</li>
-                    <li className='list-item'>Hiwi</li>
-                    <li className="list-item">Stay and Help some Old person under a program</li>
-                </ul>
-                <div className="mt-5">
-                    You will need a blocked account to enter in germany or to get a study visa for germany . This is the most important thing you need . This is the money that will cover your expenses throughout the year and this is compulsory . You cant even do ausbuildung and why you cant do it i will tell in the blog ahead
-                </div>
-                <div className="mt-5">
-                    You will not directly go to a college or university if your education system has 12 years of     education or less as europe has 13 year of education . So you will have three paths to go with
-                    <ul className='list-decimal mt-5 list-inside'>
-                        <li className='list-item'>Study in a prepatory course in germany called a Studienkolleg</li>
-                        <li className='list-item'>Do one year of bachelors in your country and then apply</li>
-                        <li className="list-item">Give a test such as JEE and get a rank if it is valid to get a admission     (confirm) its validity beforehand</li>
-                    </ul>
-                </div>
-                </div>
-
-            </div>
+            
         </div>
         </div>
         </>
